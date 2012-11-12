2c1bdcd8e855acd8868d6e2b773f73be6eb57fb1
Jeff Cohen <jeff@codeacademy.org>
2012-11-12 09:42:32 -0600

    Begin to work on user story #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Grill::Application.routes.draw do
 
-  root to: 'mockup#home'
+  resources :items
+
+  root to: 'pages#home'
+  get '/menu' => 'pages#menu', as: 'menu'
+
+
+
+  
+  get '/mockup/home' => 'mockup#home'
   
   get '/help' => 'mockup#help'
   get "/location" => 'mockup#location', as: 'location'
-  get "/menu" => 'mockup#menu', as: 'menu'
+  get "/mockup/menu" => 'mockup#menu'
   get "/orders/new" => 'mockup#ordering', as: 'new_order'
   get "/reservations/new" => 'mockup#reservations', as: 'new_reservation'
 end
