168db7c4f8ec7a911c9f18d0d083057c79d7200f
Jeff Cohen <jeff@codeacademy.org>
2012-11-16 09:04:13 -0600

    Validation for user story #4

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ActiveRecord::Base
   
   belongs_to :user
   
+  validate :less_than_max_occupancy
+  
+  def less_than_max_occupancy
+    other_people = Reservation.where(:booked_for => self.booked_for, :hour => self.hour).sum(:party_size)
+    
+    if other_people + self.party_size > 30
+      errors.add(:base, "Sorry, too many people!")
+    end
+  end
+  
 end
